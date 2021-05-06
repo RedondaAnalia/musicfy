9ad8bb021b5a3d28a70df6f9e766a65e8f452741
Redonda <A126841@bf.arg.igrupobbva>
2021-05-06 00:36:23 -0300

    Setup proyecto.(Fin 4)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import firebase from "./utils/firebase";
+import "firebase/auth";
+
 
 function App() {
+
+  firebase.auth().onAuthStateChanged(currentUser => {
+    console.log(currentUser ? "Estamos logueados" : "No estamos logueados");
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App Electron + React</h1>
+      
     </div>
   );
 }
---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBw_QUZfwQ8Mwi-6v6mpbBA0oVuEEgWsHU",
+    authDomain: "musicfy-55d9a.firebaseapp.com",
+    projectId: "musicfy-55d9a",
+    storageBucket: "musicfy-55d9a.appspot.com",
+    messagingSenderId: "159751866459",
+    appId: "1:159751866459:web:fde4d08e7a0bfa7207977e"
+  };
+
+
+export default firebase.initializeApp(firebaseConfig);
